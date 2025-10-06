19ee5b5a4aaa5605a7189e567d8d5db35f1cc212
Sebangel <angeljavicueche@gmail.com>
2025-10-06 13:40:55 -0400

    refactor: RSA keys are generated when endpoint is called

--- src/receiver/main.rs
+++ src/receiver/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder, get};
-use rsa::{RsaPrivateKey, pkcs8::{EncodePrivateKey, EncodePublicKey, LineEnding}};
+use rsa::{RsaPrivateKey, RsaPublicKey, pkcs8::{EncodePrivateKey, EncodePublicKey, LineEnding}};
 use tokio::task;
 use rand::rngs::OsRng;
 use std::sync::Arc;
@@ -21,28 +21,29 @@ async fn generate_keys_asynchronously() -> RsaPrivateKey
 }
 
 #[get("/public-key")]
-async fn get_public_key(public_key: web::Data<SharedPublicKeyPem>) -> impl Responder
+async fn get_public_key() -> impl Responder
 {
-    print!("Sending public key...\n");
-    HttpResponse::Ok().content_type("application/x-pem-file").body(public_key.to_string())
+    println!("Generating new RSA key...");
+
+    let mut private_key = generate_keys_asynchronously().await;
+    //let mut private_key_pem_string = private_key.to_public_key_pem(LineEnding::LF).expect("Failed to encode public key to PEM");
+    let private_pem = private_key.to_pkcs8_pem(LineEnding::LF).expect("Fallo la privada");
+    let mut public_key = RsaPublicKey::from(&private_key);
+    let mut public_key_pem_string = public_key.to_public_key_pem(LineEnding::LF).expect("Failed to encode public key to PEM");
+    //let shared_public_key_pem: SharedPublicKeyPem = Arc::new(public_key_pem_string.clone());
+
+    println!("{:?}",private_pem);
+    println!("Sending public key...\n");
+    HttpResponse::Ok().content_type("application/x-pem-file").body(public_key_pem_string)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>
 {
-    println!("Generating new RSA key...");
-
-    let private_key = generate_keys_asynchronously().await;
-    let public_key = private_key.to_public_key();
-    let public_key_pem_string = public_key.to_public_key_pem(LineEnding::LF).expect("Failed to encode public key to PEM");
-    let shared_public_key_pem: SharedPublicKeyPem = Arc::new(public_key_pem_string.clone());
-
-    println!("Keys generated and Public Key ({} bytes) stored.", public_key_pem_string.len());
-
     let server_address = format!("{}:{}", URL, PORT);
     println!("Key Server running at http://{}/public-key", server_address);
 
-    let server = HttpServer::new(move || {App::new().app_data(web::Data::new(shared_public_key_pem.clone()))
+    let server = HttpServer::new(move || {App::new()
         .service(get_public_key)}).bind((URL, PORT))?;
 
     clearscreen::clear().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
